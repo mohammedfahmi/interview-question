288ce36acd4143fe0859ab79079cfdf27e19932e
Mohammed Abdulla Mohammed Fahmi <mohammedfahmi606@gmail.com>
2022-06-18 09:59:17 +0200

    Update TwoSums.java

--- src/main/java/ProblemSolving/TwoSums.java
+++ src/main/java/ProblemSolving/TwoSums.java
@@ -9,25 +9,25 @@ public class TwoSums {
      *
      * Given a List of integers input and an integer target,
      * return Map of the two numbers such that they add up to target.
-     * You may assume that each input would have exactly one solution,
+     * You may assume that each element would have exactly one solution,
      * and you may not use the same element twice.
      * You can return the answer in any order.
      *
      * Example 1:
      *
      * Input: nums = [2,7,11,15], target = 9
-     * Output: [0,1]
-     * Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+     * Output: <[2,7]>
+     * Explanation: Because nums[0] + nums[1] == 9, we return <[2,7]>.
      *
      * Example 2:
      *
      * Input: nums = [3,2,4], target = 6
-     * Output: [1,2]
+     * Output: <[2,4]>
      *
      * Example 3:
      *
      * Input: nums = [3,3], target = 6
-     * Output: [0,1]
+     * Output: <[3,3]>
      *
      *
      * Constraints:
