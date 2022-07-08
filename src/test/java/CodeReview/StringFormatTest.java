472c2d6faf059a30ba04c32ee375e09f462c833b
mo-fahmi-sbs <mo.fahmi@sbs-sa.net>
2022-07-08 14:37:36 +0200

    code review branch

--- src/test/java/CodeReview/StringFormatTest.java
+++ src/test/java/CodeReview/StringFormatTest.java
@@ -11,13 +11,14 @@ class StringFormatTest {
     @Test
     public void testStringFormat() {
         //given
-        String input = "i spent on %d%s of %S %d %f %s";
+        String input = "i spent on %d%s of %s %d %f %s";
         Object[] args = List.of(11,"th", "May", 2022, 98.73, "USD").toArray();
         String expectedResult = "i spent on 11th of May 2022 98.73 USD";
         //when
         String actualOutput = STRING_FORMATTER.formatString(input, args);
         //then
         Assertions.assertEquals(expectedResult, actualOutput);
+
     }
 
 
