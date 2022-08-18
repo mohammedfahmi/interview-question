3e99201be32a0c039719659899988a61d73a9623
mo-fahmi-sbs <mo.fahmi@sbs-sa.net>
2022-08-18 16:04:13 +0200

    formatString example

---
+++ src/main/java/ProblemSolving/StringFormat.java
@@ -0,0 +1,26 @@
+package ProblemSolving;
+
+public class StringFormat {
+    /**
+     * Requirements:
+     *
+     *  0- you should interrogate the interviewer for questions about
+     *    constrains and boundary conditions
+     *
+     *  a working example
+     *      formatString(
+     *          "i spent on %d%s of %s %d %f %s", 11, "th", "May", 2022, 98.73, "USD");
+     *  returns:
+     *      "i spent on 11th of May 2022 98.73 USD"
+     *
+     *  1- your solution should be in O(n), where n is the number of characters in the input string
+     *  2- use Test Driven Development when solving this problem.
+     *  3- use SOLID principles when you can
+     *
+     *
+     * */
+    public String formatString(String input, Object... arg) {
+        String result = "";
+        return result;
+    }
+}
