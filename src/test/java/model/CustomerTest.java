b1255928f58382662b95465e7400fa0b95e38dc0
mo-fahmi-sbs <mo.fahmi@sbs-sa.net>
2022-08-18 16:17:39 +0200

    sort objects

---
+++ src/test/java/model/CustomerTest.java
@@ -0,0 +1,22 @@
+package model;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerTest {
+    List<Customer> customerListSales = new ArrayList<Customer>() {
+        {
+            add(new Customer());
+        }
+    };
+
+    @Test
+    public void sortCutomersDesendinglyByNameAndSales(){
+
+    }
+
+}
